04d4f068143de65faaec87481e896d2a783bb050
Вадим Сиваков <vadimsivakov@Mac-mini-Vadim.local>
2026-02-14 14:04:53 +0500

    Ref alert

--- src/lib/components/Alert.svelte
+++ src/lib/components/Alert.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { fly } from "svelte/transition";
 	import Button from "./Button.svelte";
     import Flex from "./Flex.svelte";
     import Icon from "./Icon.svelte";
+	import Spacer from "./Spacer.svelte";
 
     let {
         title,
@@ -16,26 +18,26 @@
     } = $props();
 
     let hover: boolean = $state(false);
-    // let show: boolean = $state(false);
     let color: string = $derived(type == 'error' ? 'text-red' : 'text-yellow');
     let glass: string = $derived(type == 'error' ? 'bg-red-glass' : 'bg-yellow-glass')
     let stroke: string = $derived(type == 'error' ? 'red' : 'yellow');
     let bg: string = $derived(type == 'error' ? 'bg-red text-gray' : 'bg-yellow text-gray');
 
-    const showing = () => show = !show;
+    const goError = () => alert();
 
 </script>
 
 <!-- svelte-ignore a11y_no_static_element_interactions -->
 <div onmouseenter={() => hover = true} onmouseleave={() => hover = false} class="w-full">
-
-    <Flex col>
-        <Button onclick={showing} className={`${hover ? bg : show ? bg : color} p-1 w-full flex items-center`}>
-            <Icon name="alert" stroke={hover ? '#323232' : show ? '#323232' : stroke} />
-            <h3 class={`${hover ? bg : show ? bg : color} p-1`}>title</h3>
+    <Flex className={`${hover ? bg : show ? bg : color} p-1 w-full flex items-center`}>
+        <Icon name="alert" stroke={hover ? '#323232' : show ? '#323232' : stroke} />
+        <h3 class={`${hover ? '' : show ? 'text-gray' : color} p-1`}>{title}</h3>
+        <Spacer />
+        <Button onclick={goError} className="p-1 hover:bg-gray hover:text-white rounded-md">
+            <Icon name="forward" />
         </Button>
-        {#if show}
-            <p class={`${show ? glass : glass} ${color} w-full p-3`}>text</p>
-        {/if}
     </Flex>
+    {#if show}
+        <p transition:fly class={`${show ? glass : glass} ${color} w-full text-start p-3`}>{text}</p>
+    {/if}
 </div>
--- src/lib/components/Alerts.svelte
+++ src/lib/components/Alerts.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
     import Alert from "./Alert.svelte";
+    import Button from "./Button.svelte";
 
     let alerts: {title: string, text: string, type: 'alert' | 'error'}[] = [
         {title: 'test', text: 'test', type: 'alert'},
-        {title: 'test', text: 'test', type: 'error'},
+        {title: 'test2', text: 'test', type: 'error'},
     ];
-    let show: number = $state(0);
+   
+    let show: number | null = $state(1000);
+
+    function toggle(i: number) {
+        show = show === i ? null : i;
+    }
 
 </script>
 
-{#each alerts as {title, text, type}, i}
-    <Alert {title} {text} {type} show={show == i}/>
+{#each alerts as {title, text, type}, i (title)}
+    <Button onclick={() => toggle(i)} className="w-full">
+        <Alert {title} {text} {type} show={show === i} />
+    </Button>
 {/each}

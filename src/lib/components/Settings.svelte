c47bff4d9c5abbeb8f77ce7b666cbaec4d7100dd
Вадим Сиваков <vadimsivakov@Mac-mini-Vadim.local>
2026-02-14 16:58:19 +0500

    create Toggle

--- src/lib/components/Settings.svelte
+++ src/lib/components/Settings.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
 	import Flex from "./Flex.svelte";
 	import User from "./User.svelte";
+    import Toggle from "./Toggle.svelte";
 
-
+    let toggles: {power: boolean, text: string}[] = [
+        {power: false, text: 'Hints'},
+        {power: true, text: 'Display command cursors'},
+        {power: true, text: 'Play audio'}
+    ];
 </script>
 
 <Flex col className="p-2">
     <User />
+    {#each toggles as {power, text}}
+        <Toggle {power} {text} />
+    {/each}
 </Flex>
---
+++ src/lib/components/Toggle.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import Button from "./Button.svelte";
+	import Flex from "./Flex.svelte";
+
+    let {
+        power = $bindable(false),
+        text
+    } : {
+        power: boolean;
+        text: string;
+    } = $props();
+
+    function toggle() {
+        power = !power;
+    }
+
+</script>
+
+<Flex className="gap-2 p-2">
+    <Button onclick={toggle} className={`${power ? 'justify-end! bg-accent' : 'justify-start! bg-black'} duration-300! p-0.5 rounded-2xl w-8 border border-gray flex justify-start`}>
+        <div class="size-3 rounded-full bg-gray"></div>
+    </Button>
+    <p class="select-none">{text}</p>
+</Flex>

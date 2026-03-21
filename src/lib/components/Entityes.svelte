d766737bdf582f0c00e983916e4b7bc658d08bc2
Вадим Сиваков <vadimsivakov@Mac-mini-Vadim.local>
2026-03-21 18:04:35 +0500

    fix selected with entities

--- src/lib/components/Entityes.svelte
+++ src/lib/components/Entityes.svelte
@@ -1,19 +1,15 @@
 <script lang="ts">
-    import { selectedStore, treeStore } from "$lib/stores/objects.svelte";
+    import { objects } from "$lib/stores/objects.svelte";
     import type { IFlatObject } from "$lib/interface";
-    import { Form } from "$lib/components";
+    import { TreeForm } from "$lib/components";
 
-    let entityes: IFlatObject[] = $derived(treeStore.all.objects ?? []);
-    
-    function onclick(id: number) {
-        selectedStore.set(id);
-    }
+    let entityes: IFlatObject[] = $derived(objects.all ?? []);
 </script>
 
 <div class="flex flex-col gap-2 items-start w-full">
-    {#each entityes as e}
-        <button onclick={() => onclick(e.id)} class="w-full">
-            <Form icon="entityes" text={e.name} />
-        </button>
+    {#each entityes as {id, name, parent}}
+        <div class="w-full flex">
+            <TreeForm {id} {name} text={name} type='o' />
+        </div>
     {/each}
 </div>
